6894e547c7976996a13b0827a45e12600e562753
gbrlferz <gbrlferz@gmail.com>
2025-04-21 18:34:04 -0300

    Implement advanced air movement

--- fps_controller/fps_controller.gd
+++ fps_controller/fps_controller.gd
@@ -15,6 +15,12 @@ const HEADBOB_MOVE_AMOUNT := 0.06
 const HEADBOB_FREQUENCY := 2.04
 var headbob_time := 0.0
 
+# Air movement settings. Need to tweak these to get the feeling dialed in.
+@export_group("Air movement")
+@export var air_cap := 0.85 ## Can surf steeper ramps if this is higher, makes it easier to stick and bhop.
+@export var air_accel := 800.0
+@export var air_move_speed := 500.0
+
 var wish_dir := Vector3.ZERO
 
 
@@ -75,6 +81,21 @@ func _handle_ground_physics(delta: float) -> void:
 func _handle_air_physics(delta: float) -> void:
 	self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
 	# self.velocity -= get_gravity()
+
+	# Classic battle tested & fan favorite source/quake air movement
+	# CSS players gonna feel their gamer instincts kick in with this one
+	var cur_speed_in_wish_dir := self.velocity.dot(wish_dir) # Dot product of velocity and the wish direction.
+	# Wish speed (if wish_dir > 0 length) capped to air_cap
+	var capped_speed: float = min((air_move_speed * wish_dir).length(), air_cap)
+	# How much to get to the speed the player wishes (in the new dir)
+	# Notice this allows for infinite speed. If wish_dir is perpendicular, we always need to add velocity
+	#  no matter how fast we are going. This is what allows for things like bhop in CSS & Quake.
+	# Also happens to just give some very nice feeling movement & responsiveness when in air.
+	var add_speed_till_cap := capped_speed - cur_speed_in_wish_dir
+	if add_speed_till_cap > 0:
+		var accel_speed := air_accel * air_move_speed * delta
+		accel_speed = min(accel_speed, add_speed_till_cap)
+		self.velocity += accel_speed * wish_dir
 
 
 func _physics_process(delta: float) -> void:
